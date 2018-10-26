166ca5832e868f3fb915c4a47549e519a722b10d
関悠史 <yushiseki@i-am-MacBook-Air.local>
2018-10-26 18:39:10 +0900

    deploy-fix

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,4 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :blogs, inverse_of: :admin, dependent: :destroy
