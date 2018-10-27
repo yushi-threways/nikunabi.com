e37bd0269268c8ad782687458ddfa6f387197b0b
関悠史 <yushiseki@i-am-MacBook-Air.local>
2018-10-27 14:29:26 +0900

    deploy-fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "133.167.104.12", user: "threwaysuser", roles: %w{app db web}, port: 22
+server "133.167.104.12", user: "threwaysuser", roles: %w{app db web}, port: 5710
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
