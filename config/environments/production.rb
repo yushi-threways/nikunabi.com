6e0694c60d0c85aa4c6aa26519a127ac951e9ade
関悠史 <yushiseki@i-am-MacBook-Air.local>
2018-11-06 13:43:42 +0900

    deviceのmail設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp18.gmoserver.jp", # smtpサーバーのホスト名
+    :port => 587,
+    :authentication => :plain,
+    :user_name => "nikunabi@nagoya1.work",
+    :password => "fq46#LBv"
+}
 end
