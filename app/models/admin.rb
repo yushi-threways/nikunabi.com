5c3f9dd7b0f1cfb9af3bfc7642ce299612d3915a
関悠史 <yushiseki@i-am-MacBook-Air.local>
2018-11-29 17:41:03 +0900

    admin部分の変更に伴い修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,6 @@
 class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :blogs, inverse_of: :admin, dependent: :destroy
-  accepts_nested_attributes_for :blogs, allow_destroy: true
-  has_many :informations, inverse_of: :admin, dependent: :destroy
-  accepts_nested_attributes_for :informations, allow_destroy: true
+  has_many :blogs, dependent: :destroy
+  has_many :informations, dependent: :destroy
 end
--- app/models/information.rb
+++ app/models/information.rb
@@ -1,3 +1,8 @@
 class Information < ApplicationRecord
     belongs_to :admin
+
+    validates :title, presence: true, length: { in: 3..50 } 
+    validates :content, presence: true
+
 end
+
