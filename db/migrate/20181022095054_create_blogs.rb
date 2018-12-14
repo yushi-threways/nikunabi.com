11d3a088d8d16b12c7cdd4de73bcab7bb59d8250
関悠史 <yushiseki@i-am-MacBook-Air.local>
2018-12-14 19:26:36 +0900

    recommendのcontent部分を削除/ブログ一覧ページの作成/ブログ詳細ページの変更/ブログテーブルの修正

--- db/migrate/20181022095054_create_blogs.rb
+++ db/migrate/20181022095054_create_blogs.rb
@@ -3,13 +3,9 @@ class CreateBlogs < ActiveRecord::Migration[5.2]
     create_table :blogs do |t|
       t.references :admin, foreign_key: true
       t.string :title
-      t.string :secondtitle
       t.string :subtitle
-      t.string :secondsubtitle
       t.text   :content
-      t.text   :secondcontent
       t.string :image
-      t.string :secondimage
 
       t.timestamps
     end
