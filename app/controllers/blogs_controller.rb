6a4512b645126d5aa650c4d87a0b95ec4db77bcc
vagrant <vagrant@localhost.localdomain>
2019-02-26 02:44:23 +0000

    modify-blog-design

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.order(created_at: :desc) 
   end
 
   def show
